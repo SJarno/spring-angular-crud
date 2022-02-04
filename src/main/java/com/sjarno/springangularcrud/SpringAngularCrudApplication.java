d063060608c1e60cec9c060666f597a57c0491fa
SJarno <jarno.saastamoinen@hotmail.com>
2022-02-04 10:25:44 +0200

    spring boot project

---
+++ src/main/java/com/sjarno/springangularcrud/SpringAngularCrudApplication.java
@@ -0,0 +1,13 @@
+package com.sjarno.springangularcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAngularCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAngularCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sjarno/springangularcrud/SpringAngularCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sjarno.springangularcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAngularCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
