5ddd0730e4fcdab4a4da12a400da53acd70e986b
SJarno <jarno.saastamoinen@hotmail.com>
2022-02-04 15:33:55 +0200

    fix greeting test

--- src/test/java/com/sjarno/springangularcrud/controllers/TodoControllerTest.java
+++ src/test/java/com/sjarno/springangularcrud/controllers/TodoControllerTest.java
@@ -28,7 +28,6 @@ import com.sjarno.springangularcrud.repository.TodoRepository;
 
 import static org.hamcrest.Matchers.*;
 
-
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @ActiveProfiles("test")
@@ -61,7 +60,7 @@ public class TodoControllerTest {
         this.todoSecond = new Todo("TitleTwo", "ContentTwo");
         this.todoThird = new Todo("TitleThree", "ContentThree");
         todoList = new ArrayList<>(Arrays.asList(
-            todoFirst, todoSecond, todoThird));
+                todoFirst, todoSecond, todoThird));
         this.todoRepository.saveAll(todoList);
 
         this.todoWithNullVals = new Todo();
@@ -69,6 +68,7 @@ public class TodoControllerTest {
         this.todoWithContentOnly = new Todo("", "ContentOnly");
 
     }
+
     @AfterEach
     private void tearDown() {
         this.todoRepository.deleteAll();
@@ -78,88 +78,92 @@ public class TodoControllerTest {
     void testGreeting() throws Exception {
         MvcResult result = this.mockMvc.perform(get("/api/greet"))
                 .andExpect(status().isOk()).andReturn();
-        assertEquals("Heippa maailma!", result.getResponse().getContentAsString());
+        String jsonContent = "{\"greet\":\"Hello, this is a greeting from the server!\"}";
+        assertEquals(jsonContent, result.getResponse().getContentAsString());
     }
+
     /* Initial size: */
     @Test
     void testInitialSize() {
         assertEquals(3, todoRepository.findAll().size());
     }
+
     @Test
     void testAddingNewTodo() throws Exception {
-        
+
         Todo newTodo = new Todo("newTitle", "newContent");
         MvcResult result = addNewTodo(newTodo)
-            .andExpect(status().isOk())
-            .andReturn();
+                .andExpect(status().isOk())
+                .andReturn();
         String jsonContent = "{\"id\":4,\"title\":\"newTitle\",\"content\":\"newContent\",\"new\":false}";
         assertEquals(jsonContent, result.getResponse().getContentAsString());
         assertEquals(4, todoRepository.findAll().size());
     }
+
     /* Test errors here: */
     void addNewTodoWithWrongValues() {
 
     }
-    
 
     @Test
     void testAllTodos() throws Exception {
         MvcResult result = this.mockMvc.perform(get("/api/todos"))
-            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-            .andExpect(status().isOk())
-            .andExpect(jsonPath("$[*].id", 
-                containsInAnyOrder(1, 2, 3)))
-            .andExpect(jsonPath("$[*].title", 
-                containsInAnyOrder("TitleOne", "TitleTwo", "TitleThree")))
-            .andExpect(jsonPath("$[*].content", 
-                containsInAnyOrder("ContentOne", "ContentTwo", "ContentThree")))
-            .andReturn();
-        //if need to check whole string:
-        //assertEquals("expected", result.getResponse().getContentAsString());
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[*].id",
+                        containsInAnyOrder(1, 2, 3)))
+                .andExpect(jsonPath("$[*].title",
+                        containsInAnyOrder("TitleOne", "TitleTwo", "TitleThree")))
+                .andExpect(jsonPath("$[*].content",
+                        containsInAnyOrder("ContentOne", "ContentTwo", "ContentThree")))
+                .andReturn();
+        // if need to check whole string:
+        // assertEquals("expected", result.getResponse().getContentAsString());
     }
+
     @Test
     void testGetTodId() throws Exception {
         MvcResult result = getTodoById(1, "TitleOne", "ContentOne")
-            .andExpect(status().isOk()).andReturn();
+                .andExpect(status().isOk()).andReturn();
         String content = "{\"id\":1,\"title\":\"TitleOne\",\"content\":\"ContentOne\",\"new\":false}";
         assertEquals(content, result.getResponse().getContentAsString());
     }
+
     @Test
-    void canUpdateTodo() throws Exception{
+    void canUpdateTodo() throws Exception {
         Todo updatedValues = new Todo("TitleUpdated", "ContentUpdated");
         MvcResult result = this.mockMvc.perform(MockMvcRequestBuilders.put("/api/update/2")
-            .contentType(MediaType.APPLICATION_JSON)
-            .content(objectMapper.writeValueAsString(updatedValues)))
-            .andExpect(status().isOk()).andReturn();
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(updatedValues)))
+                .andExpect(status().isOk()).andReturn();
         String jsonContent = "{\"id\":2,\"title\":\"TitleUpdated\",\"content\":\"ContentUpdated\",\"new\":false}";
         assertEquals(jsonContent, result.getResponse().getContentAsString());
         assertEquals(3, this.todoRepository.findAll().size());
     }
+
     @Test
     void canDeleteTodoById() throws Exception {
         MvcResult result = this.mockMvc.perform(MockMvcRequestBuilders.delete("/api/delete/{id}", 2))
-            .andExpect(status().isOk()).andReturn();
-            String jsonContent = "{\"id\":2,\"title\":\"TitleTwo\",\"content\":\"ContentTwo\",\"new\":false}";
+                .andExpect(status().isOk()).andReturn();
+        String jsonContent = "{\"id\":2,\"title\":\"TitleTwo\",\"content\":\"ContentTwo\",\"new\":false}";
         assertEquals(jsonContent, result.getResponse().getContentAsString());
         assertEquals(2, this.todoRepository.findAll().size());
     }
 
     private ResultActions addNewTodo(Todo todo) throws Exception {
         return this.mockMvc.perform(post(("/api/add-todo"))
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .content(objectMapper.writeValueAsString(todo)));
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(objectMapper.writeValueAsString(todo)));
     }
 
-    private ResultActions getTodoById(int id, String title, String content) throws Exception{
-        return this.mockMvc.perform(get("/api/todos/"+id)
-            .contentType(MediaType.APPLICATION_JSON_VALUE))
-            .andExpect(jsonPath("$.id", is(id)))
-            .andExpect(jsonPath("$.title", is(title)))
-            .andExpect(jsonPath("$.content", is(content)));
-            //.andExpect(status());
-            
-            
+    private ResultActions getTodoById(int id, String title, String content) throws Exception {
+        return this.mockMvc.perform(get("/api/todos/" + id)
+                .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$.id", is(id)))
+                .andExpect(jsonPath("$.title", is(title)))
+                .andExpect(jsonPath("$.content", is(content)));
+        // .andExpect(status());
+
     }
 
-    
 }
